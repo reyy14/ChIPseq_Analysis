2bcdad7a4a32a229038c92b72038b3090c8e88b3
Reynold Yu <reyy@sas.upenn.edu>
2016-04-10 04:17:15 -0400

    Delete R1.TSS.R

--- R1.TSS.R
+++
@@ -1,6 +0,0 @@
-refFlat = read.delim("/home/won/Local/mm9/refFlat.txt",header=F)
-#3 chromosome 4 strand 5 start 6 end
-ann = read.delim("/home/skw/Desktop/REy/Ben_Rey/H3S10ac_A_8_23_ann.txt",row.names=1,header=T)
-
-peak = read.delim("/home/skw/Desktop/REy/Ben_Rey/H3S10ac_A_8_23_sorted.homer/peakscut.txt",row.names=1,header=T)
-
